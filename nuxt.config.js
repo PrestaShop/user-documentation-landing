6b6dc0421d89862e1524b5391f13078ffcffefb3
NeOMakinG <valentin.szczupak@prestashop.com>
2020-12-22 16:41:26 +0100

    Add cd

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
 
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
+  router: {
+    base: '/user-documentation-landing/'
+  },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
